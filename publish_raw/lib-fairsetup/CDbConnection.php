e6414a24d8385ead0c928666e8f53bf2a6a68981
Nikita <me@nikitab.com>
2013-07-26 16:32:11 -0400

    DB

--- publish_raw/lib-fairsetup/CDbConnection.php
+++ publish_raw/lib-fairsetup/CDbConnection.php
@@ -15,7 +15,7 @@ class CDBConnection
 		$is_production = file_exists( "../../../production_tag.txt" );
 		if( $is_production ){
 			$serverName 		= "WIN-8839QEJ1X7K";
-			$connectionInfo 	= array("Database" => "FairSetup" );  // For local connections
+			$connectionInfo 	= array("Database" => "FairSetup_staging" );  // For local connections
 		}
 		else{
 			$serverName 		= "NIKITA-X200";
