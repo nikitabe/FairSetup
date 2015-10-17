baae0b83627df941453216081b4e8a8205754e9e
Nikita <me@nikitab.com>
2015-10-17 13:52:48 -0400

    refactor + throttle issue

--- publish_raw/lib-fairsetup/CDbConnection.php
+++ publish_raw/lib-fairsetup/CDbConnection.php
@@ -22,7 +22,7 @@ class CDBConnection
 		else{
 			$serverName 		= "X200-NIKITA";
 			//$connectionInfo 	= array("Database" => "FairSetup_2014_06_13_dev" );  // For local connections
-			$connectionInfo 	= array("Database" => "FairSetup_2014_10_25_prod" );  // For local connections
+			$connectionInfo 	= array("Database" => "FairSetup_2015_09_19_4_prod" );  // For local connections
 		}
 		$this->conn = sqlsrv_connect( $serverName, $connectionInfo );
 		if( $this->conn === false )
